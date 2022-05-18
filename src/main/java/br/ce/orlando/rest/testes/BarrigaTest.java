de483537aee05edb3aaa3e1e19a538bdb6c0f4b8
Orlando <orlando.junior07@hotmail.com>
2022-05-17 23:39:00 -0300

    Deve incluir conta com sucesso

--- src/main/java/br/ce/orlando/rest/testes/BarrigaTest.java
+++ src/main/java/br/ce/orlando/rest/testes/BarrigaTest.java
@@ -2,6 +2,9 @@ package br.ce.orlando.rest.testes;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import br.ce.orlando.rest.core.BaseTest;
@@ -16,7 +19,34 @@ public class BarrigaTest extends BaseTest{
 			.get("/contas")
 		.then()
 			.statusCode(401)
-			
+		;
+	}
+	
+	
+	@Test
+	public void deveIncluirContaComSucesso() {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "orlando.dev@hotmail.com");
+		login.put("senha", "1234");
+		
+		String token = given()
+		.when()
+			.log().all()
+			.body(login)
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+		;
+		
+		given()
+			.header("Authorization", "JWT " + token)
+			.body("{ \"nome\": \"uma conta\" }")
+			.log().all()
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)
 		;
 	}
 	
