71f015a8179eaa35326fddb0c1cd9032814aa21d
Orlando <orlando.junior07@hotmail.com>
2022-05-19 23:38:30 -0300

    Deve alterar conta com sucesso

--- src/main/java/br/ce/orlando/rest/testes/BarrigaTest.java
+++ src/main/java/br/ce/orlando/rest/testes/BarrigaTest.java
@@ -5,11 +5,31 @@ import static io.restassured.RestAssured.given;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.ce.orlando.rest.core.BaseTest;
 
 public class BarrigaTest extends BaseTest{
+	
+	private String TOKEN;
+	
+	@Before
+	public void login() {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "orlando.dev@hotmail.com");
+		login.put("senha", "1234");
+		
+		TOKEN = given()
+		.when()
+			.log().all()
+			.body(login)
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token");
+	}
 	
 	@Test
 	public void naoDeveAcessarAPISemToken() {
@@ -25,28 +45,32 @@ public class BarrigaTest extends BaseTest{
 	
 	@Test
 	public void deveIncluirContaComSucesso() {
-		Map<String, String> login = new HashMap<String, String>();
-		login.put("email", "orlando.dev@hotmail.com");
-		login.put("senha", "1234");
-		
-		String token = given()
-		.when()
-			.log().all()
-			.body(login)
-			.post("/signin")
-		.then()
-			.statusCode(200)
-			.extract().path("token")
-		;
-		
 		given()
-			.header("Authorization", "JWT " + token)
+			.header("Authorization", "JWT " + TOKEN )
 			.body("{ \"nome\": \"uma conta\" }")
 			.log().all()
 		.when()
 			.post("/contas")
 		.then()
 			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void deveAlterarContaComSucesso() {
+		given()
+			.header("Authorization", "JWT " + TOKEN)
+			.body("{ \"nome\": \"conta alterada\" }")
+			.log().all()
+		.when()
+			.put("/contas/1177425")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("nome", Matchers.is("conta alterada"))
+			.body("visivel", Matchers.is(true))
+			.body("id", Matchers.is(1177425))
+			.body("usuario_id", Matchers.is(30060))
 		;
 	}
 	
